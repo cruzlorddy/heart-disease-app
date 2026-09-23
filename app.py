ae35662ea6a25630f119a443d244d0be870650d3
CRUZ LORD TEK <destinyfrosh952@gmail.com>
2026-09-23 15:04:40 +0100

    Enable CORS for Flask application
    Added CORS support to the Flask app.

--- app.py
+++ app.py
@@ -2,8 +2,10 @@
 from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 model = pickle.load(open('heart_model.pkl', 'rb'))
 
 @app.route('/predict', methods=['POST'])
